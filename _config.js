7a6ca0299cc28a954afbcf08a519a2a50d8f6342
muturi254 <petermuturi74@gmail.com>
2025-04-03 08:57:22 +0300

    add test

--- _config.js
+++ _config.js
@@ -2,8 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://muturi:muturi254@cluster0.rfjh5w5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://muturi:muturi254@cluster0.rfjh5w5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://muturi:muturi254@cluster0.rfjh5w5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    // production: 'mongodb+srv://muturi:muturi254@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    // development: 'mongodb+srv://muturi254:muturi254@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    // test: 'mongodb+srv://muturi254:muturi254@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
